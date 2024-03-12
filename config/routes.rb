20ba5e2275bf0059273fe1be1a696ff15ed73afb
choosenname <vlad@luckashonok.ru>
2024-03-12 09:55:07 +0300

    feat: show profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/signup', to: 'users#new'
-  resources :users, only: [:new, :create]
+  resources :users
 end
