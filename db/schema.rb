79fd229098f54c4d3c424b2bbea6a844b29f69ac
choosenname <vlad@luckashonok.ru>
2024-03-12 13:33:42 +0300

    feat: adding a categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_12_073321) do
+ActiveRecord::Schema.define(version: 2024_03_12_102454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name", null: false
@@ -22,6 +29,8 @@ ActiveRecord::Schema.define(version: 2024_03_12_073321) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_events_on_category_id"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
@@ -35,5 +44,6 @@ ActiveRecord::Schema.define(version: 2024_03_12_073321) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "events", "categories"
   add_foreign_key "events", "users"
 end
